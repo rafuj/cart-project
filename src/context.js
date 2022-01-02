92d3afbfc5b823d6130fa19b5366d0249ea39fc9
rafuj <raihanrafuj0078@gmail.com>
2022-01-02 21:47:15 +0600

    Complete UI Part

---
+++ src/context.js
@@ -0,0 +1,26 @@
+import React, { useContext, useState } from "react";
+import cartItems from "./data";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [cart, setCart] = useState(cartItems);
+
+  return (
+    <AppContext.Provider
+      value={{
+        cart,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
